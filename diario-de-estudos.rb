094dc82498843a8491c9e71355aba3a5a48b11b4
Lótus Inaiê <lotus.inaie@gmail.com>
2021-11-19 22:37:30 -0700

    Ainda não usei nem POO, mas direcionei melhor qual a lógica de cada um dos comandos básicos!

--- diario-de-estudos.rb
+++ diario-de-estudos.rb
@@ -1,5 +1,7 @@
 require "io/console"
 
+itens = []
+
 loop do
   puts `clear`
   puts "Olá, eu sou o seu diário de estudos particular e estou aqui para ajudar!
@@ -14,11 +16,27 @@ loop do
   entrada = gets.chomp.strip
   case entrada
   when "1"
-    puts "Ações do #{entrada} aqui!"
+    puts "Digite o nome do item que você quer cadastrar!"
+    item = gets.chomp.strip
+    puts "Qual é a categoria de #{item}?"
+    puts "[1] - Lógica em Ruby"
+    puts "[2] - POO em Ruby"
+    puts "[3] - Git e Terminal"
+    categoria = gets.chomp.strip
+    puts "O item #{item} é da categoria #{categoria}!"
+    itens.push("#{categoria} - #{item}")
   when "2"
-    puts "Ações do #{entrada} aqui!"
+    puts "Os itens que estão cadastrados atualmente são:"
+    puts itens
   when "3"
-    puts "Ações do #{entrada} aqui!"
+    puts "Digite alguma palavra chave para a sua pesquisa!"
+    pesquisa = gets.chomp.strip.downcase
+    puts "Segue(m) o(s) item(ns) com a palavra chave especificada:"
+    for item in itens
+      if item.downcase.include?(pesquisa)
+        puts item
+      end
+    end
   when "4"
     puts "Já está tudo finalizado e pronto para nos despedirmos! Até mais!"
     break
